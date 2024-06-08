e26d931b1f02f5ebb92cf8d75239243cbdea4956
lezz100 <preciouskipkorir374@gmail.com>
2024-06-09 00:32:41 +0300

    first commit

---
+++ user_input.py
@@ -0,0 +1,15 @@
+name = input("What is your name? ")
+
+# You can now use the name variable
+print(f"Hello, {name}!")
+
+
+age = input("How old are you")
+print(f"I am {age} years old")
+
+location = input("I stay in   ")
+print(f"I stay in {location}" )
+
+message = f"Hello {name}, you are {age} years old and live in {location}."
+
+print(message)
